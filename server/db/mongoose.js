2abf9e9e9ebf13df6f7da2a19740a72414bcc186
Thanat V <thanatv@thaicom.net>
2017-09-03 11:02:44 +0700

    Add POST /todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+// Tell mongoose we want to use built-in promise, not 3rd party promise
+mongoose.Promise = global.Promise;
+
+// Mongoose maintains connection for us, no need to use callback like MongoClient
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {mongoose};
